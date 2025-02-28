78c042e558c10e963df26c29260a50ec6888b234
Mingyu Jang <disrepair1825@gmail.com>
2025-03-01 01:50:16 +0900

    기초 GUI 프로그래밍
    기초 GUI 프로그래밍 실습을 진행했습니다.

---
+++ ch6/simpleGUI.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtWidgets import *
+import sys
+import cv2 as cv
+
+class videoGUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Video GUI')
+        self.setGeometry(200,200,500,100)
+        
+        videoButton = QPushButton('비디오 켜기',self)
+        captureButton = QPushButton('프레임 잡기',self)
+        saveButton = QPushButton('프레임 저장',self)
+        quitButton = QPushButton('나가기',self)
+        
+        videoButton.setGeometry(10,10,100,30)
+        captureButton.setGeometry(110,10,100,30)
+        saveButton.setGeometry(210,10,100,30)
+        quitButton.setGeometry(310,10,100,30)
+        
+        videoButton.clicked.connect(self.videoFunction)
+        captureButton.clicked.connect(self.captureFunction)
+        saveButton.clicked.connect(self.saveFunction)
+        quitButton.clicked.connect(self.quitButtonFunction)
+        
+    def videoFunction(self):
+        self.cap = cv.VideoCapture(0,cv.CAP_DSHOW)
+        if not self.cap.isOpened(): self.close()
+
+        while True:
+            ret,self.frame=self.cap.read()
+            if not ret: break
+            cv.imshow('video display',self.frame)
+            cv.waitKey(1)
+            
+    
+    def captureFunction(self):
+        self.capturedFrame = self.frame
+        cv.imshow('Captured frame',self.capturedFrame)
+        
+    def saveFunction(self):
+        fname=QFileDialog.getSaveFileName(self,'파일 저장','./',self.tr("Images (*.jpg *.gif *.png *.xpm );; All Files(*.*)"))
+        print(fname)
+        cv.imwrite(fname[0],self.capturedFrame)
+        
+    def quitButtonFunction(self):
+        if self.cap.isOpened(): self.cap.release()
+        cv.destroyAllWindows()
+        self.close()
+
+    
+app = QApplication(sys.argv)
+win = videoGUI()
+win.show()
+app.exec_()
